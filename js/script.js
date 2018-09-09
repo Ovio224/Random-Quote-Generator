bc8a1fd1c25c9a425c43746c7a47e87c40841dc8
Ovi# <covidiu97@gmail.com>
2018-09-09 13:22:33 +0200

    Added auto-refresh and one more property to the quotes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 const quotes = [
     { quote: "Don't cry because it's over, smile because it happened.", source: "Dr. Seuss"},
     { quote: "Be yourself, everyone else is already taken.", source: "Oscar Wilde"},
-    { quote: "Two things are infinite: the universe and human stupidity, and I'm not sure about the universe. ", source: "Albert Einstein"},
+    { quote: "Two things are infinite: the universe and human stupidity, and I'm not sure about the universe. ", source: "Albert Einstein", tag: "humble"},
     { quote: "You only live once, but if you do it right, once is enough.", source: "Mae West"},
     { quote: "Be the change that you wish to see in the world.", source: "Mahatma Gandhi" },
     { quote: "Without music, life would be a mistake.", source: "Friedrich Nietzsche", citation: "Twilight of the Idols"},
@@ -11,14 +11,11 @@ const quotes = [
     { quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.", source: "Ralph Waldo Emerson", year: 1828 }
 ];
 
-
 // Create the getRandomQuuote function and name it getRandomQuote
-
 function getRandomQuote (array) {
         let randomValue = quotes[Math.floor(Math.random()*quotes.length)];
         return randomValue;
 }
-
 // Create the printQuote funtion and name it printQuote
 let theString;
 var aux
@@ -26,6 +23,9 @@ function printQuote () {
    aux = getRandomQuote(quotes);
    theString = '<p class="quote">' + aux.quote + '</p>';
    theString = theString +'<p class="source">' + aux.source;
+   if (typeof aux.tag === "string") {
+     theString = theString + '<span class="citation">' + aux.tag + '</span>';
+   }
    if (typeof aux.citation === "string") {
      theString = theString + '<span class="citation">' + aux.citation + '</span>';
    }
@@ -37,9 +37,7 @@ function printQuote () {
    document.getElementById('quote-box').innerHTML = theString;
    randomColor();
 }
-
 //Random background color
-
 function randomColor () {
   var r = Math.floor(Math.random() * 256);
   var g = Math.floor(Math.random() * 256);
@@ -48,9 +46,14 @@ function randomColor () {
   document.body.style.backgroundColor = rndColor;
 }
 
+// Auto-refresh evert 10 seconds
+var intervalId = window.setInterval(printQuote, 10000)
+
+//Reset timer
+function timeOut() {
+  window.clearTimeout(intervalId);
+}
+
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-  if(printQuote = true) {
-    theString = '<div style="background-color: blue"></div>'
-  }
